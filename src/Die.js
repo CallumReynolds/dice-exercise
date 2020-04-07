8c2753152ad3b4c065a2906bb6d06dbfb1bdae17
Callum Reynolds <callum_rey@hotmail.com>
2020-04-07 21:06:43 +1000

    animation

--- src/Die.js
+++ src/Die.js
@@ -4,7 +4,7 @@ import './Die.css';
 class Die extends Component {
     render(){   
         return(
-            <i className={`Die-icon fas fa-dice-${this.props.number}`}></i>
+            <i className={`Die-icon fas fa-dice-${this.props.number} ${this.props.rolling && 'shaking'}`}></i>
         );
     }
 }
--- src/RollDice.js
+++ src/RollDice.js
@@ -6,7 +6,7 @@ class RollDice extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isRolled: true,
+            rolling: false,
             num1: 'six',
             num2: 'six' 
         }
@@ -24,16 +24,21 @@ class RollDice extends Component {
     roll(e){
         let item1 = this.random(this.props.diceNum);
         let item2 = this.random(this.props.diceNum);
-        this.setState({num2 : item2, num1 : item1});
+        this.setState({num2 : item2, num1 : item1, rolling: true});
+        setTimeout(() => {
+           this.setState({rolling: false}) 
+        }, 1000);
     }
 
     render(){
         return(
             <div>
-                <Die number={this.state.num1}/>
-                <Die number={this.state.num2}/>
+                <Die number={this.state.num1} rolling={this.state.rolling}/>
+                <Die number={this.state.num2} rolling={this.state.rolling}/>
                 <br/>
-                <button className="RollDice-button" onClick={this.roll}>Roll Dice~!</button>
+                <button className="RollDice-button" onClick={this.roll} disabled={this.state.rolling}>
+                   {this.state.rolling ? "Rolling..." : "Roll dice~!"}
+                </button>
             </div>
         ); 
     }
